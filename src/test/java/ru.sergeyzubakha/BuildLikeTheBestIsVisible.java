abaaad9692531388a8cfcd050e9001df097c7821
Sergey Zubakha <qasergeyzubakha@yandex.ru>
2023-06-11 22:17:34 +0300

    homeWork

---
+++ src/test/java/ru.sergeyzubakha/BuildLikeTheBestIsVisible.java
@@ -0,0 +1,17 @@
+package ru.sergeyzubakha;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class BuildLikeTheBestIsVisible {
+    @Test
+    void BuildLikeTheBestIsVisible() {
+        open("https://github.com/");
+        $((byText("Solutions"))).hover();
+        $("[href=\"/enterprise\"").click();
+        $(".application-main").shouldHave(text("Build like the best"));
+    }
+}
